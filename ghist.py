015d5d3afd9a2501c4eab267d9ea03a35406e4bd
Gary M. Josack <gary@byoteki.com>
2021-01-20 21:09:00 -0800

    Add config to restrict bot to specific channels

--- ghist.py
+++ ghist.py
@@ -1,12 +1,20 @@
 import argparse
 import asyncio
+import json
 import logging
 import os
+from collections import defaultdict
+from pathlib import Path
 
 from discord.ext import commands
 
 TOKEN = os.environ["GHIST_BOT_TOKEN"]
 COLOR_PREFIX = "Color: "
+
+# Mapping of guild to list of channels where the bot will respond.
+# If guild not found or channel list is empty then the bot will
+# respond in all channels.
+VALID_CHANNELS = defaultdict(list)
 
 
 def get_colors_roles(roles):
@@ -65,21 +73,48 @@ async def globally_block_dms(ctx):
     return ctx.guild is not None
 
 
+async def check_valid_channels(ctx):
+    if ctx.guild is None:
+        return False
+
+    channels = VALID_CHANNELS.get(str(ctx.guild.id))
+    if not channels:
+        return True
+
+    return str(ctx.message.channel.id) in channels
+
+
+def parse_config(config_path):
+    with config_path.open("r") as config_file:
+        data = json.load(config_file)
+
+    for guild, channels in data.get("valid-channels", {}).items():
+        VALID_CHANNELS[guild] = channels
+
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "--prefix", default="!", help="The prefix this bot uses on bot commands."
     )
+    parser.add_argument(
+        "--config",
+        default=Path(__file__).absolute().parent / "ghist-bot-config.json",
+        type=Path,
+        help="Path to config file.",
+    )
     args = parser.parse_args()
 
+    if args.config.exists():
+        parse_config(args.config)
+
     # Change only the no_category default string
-    help_command = commands.DefaultHelpCommand(
-        no_category = 'Commands'
-    )
+    help_command = commands.DefaultHelpCommand(no_category="Commands")
 
     ghist = commands.Bot(command_prefix=args.prefix, help_command=help_command)
     ghist.add_command(color)
     ghist.add_check(globally_block_dms)
+    ghist.add_check(check_valid_channels)
     ghist.run(TOKEN)
 
 
