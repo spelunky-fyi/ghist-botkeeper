9f632ade3088355cc26282df4a821cd85125ff9a
Gary M. Josack <gary@byoteki.com>
2021-01-21 17:15:07 -0800

    Add support for syncing game specific roles

--- ghist.py
+++ ghist.py
@@ -1,14 +1,18 @@
 import argparse
 import asyncio
-import aiohttp
 import json
 import logging
 import os
+import re
 from collections import defaultdict
+from dataclasses import dataclass
 from pathlib import Path
+from typing import Dict
 
+import aiohttp
 import discord
 from discord.ext import commands, tasks
+
 
 TOKEN = os.environ["GHIST_BOT_TOKEN"]
 MR_SYNC_KEY = os.environ["MR_SYNC_KEY"]
@@ -20,17 +24,63 @@ COLOR_PREFIX = "Color: "
 VALID_CHANNELS = defaultdict(list)
 
 MR_SYNC_ENDPOINT = "https://mossranking.com/api/getdiscordusers.php"
+GAMES_RE = re.compile(r"^games\[([A-Za-z0-9 ]+)\]$")
+
+
+@dataclass
+class MossRecord:
+    """Represents a record from the Mossranking getdiscordusers endpoint.
+
+    Example Payload:
+    [
+      {
+        "mossranking[id_user]":"1",
+        "mossranking[username]":"somename",
+        "discord[id]":"6666666666666666",
+        "discord[username]":"somediscordname",
+        "games[Spelunky Classic]":true,
+        "games[Spelunky HD]":true,
+        "games[Spelunky 2]":true,
+        "games[Roguelike Challenges]":true
+      }
+    ]
+    """
+
+    mossranking_id: int
+    mossranking_username: str
+    discord_id: int
+    discord_username: str
+    games: Dict[str, bool]
+
+    @classmethod
+    def from_dict(cls, data):
+        games = {}
+
+        for key, value in data.items():
+            match = GAMES_RE.match(key)
+            if match:
+                games[match.group(1)] = value
+
+        return cls(
+            mossranking_id=int(data.get("mossranking[id_user]", 0)),
+            mossranking_username=data.get("mossranking[username]", ""),
+            discord_id=int(data.get("discord[id]", 0)),
+            discord_username=data.get("discord[username]", ""),
+            games=games,
+        )
 
 
 class MossrankingSync(commands.Cog):
-    def __init__(self, bot, guild_id, role_id):
+    def __init__(self, bot, guild_id, role_id, game_role_ids):
         self.bot = bot
         self.guild_id = guild_id
         self.role_id = role_id
+        self.game_role_ids = game_role_ids
+
         self.syncer.start()  # pylint: disable=no-member
 
     async def get_mr_discord_users(self):
-        user_ids = set()
+        records = {}
         async with aiohttp.ClientSession() as session:
             async with session.get(
                 MR_SYNC_ENDPOINT, params={"key": MR_SYNC_KEY}
@@ -38,10 +88,23 @@ class MossrankingSync(commands.Cog):
                 if req.status != 200:
                     return
                 data = await req.json()
-        for user in data:
-            user_ids.add(int(user["discord[id]"]))
 
-        return user_ids
+        for user in data:
+            record = MossRecord.from_dict(user)
+            if not record.discord_id:
+                continue
+            records[record.discord_id] = record
+
+        return records
+
+    def get_games_roles(self, guild):
+        roles = {}
+        for game, role_id in self.game_role_ids.items():
+            role = guild.get_role(role_id)
+            if role is None:
+                continue
+            roles[game] = role
+        return roles
 
     @tasks.loop(seconds=1800.0)
     async def syncer(self):
@@ -53,20 +116,54 @@ class MossrankingSync(commands.Cog):
         if not role:
             return
 
-        mr_discord_ids = await self.get_mr_discord_users()
+        game_roles = self.get_games_roles(guild)
+
+        mr_records_by_did = await self.get_mr_discord_users()
         # Safety check in case api returns empty data
-        if not mr_discord_ids:
+        if not mr_records_by_did:
             return
 
         for member in guild.members:
-            if member.id in mr_discord_ids:
+            to_add = []
+            to_remove = []
+            mr_record = mr_records_by_did.get(member.id)
+
+            if mr_record:
                 if role not in member.roles:
-                    logging.info("Added role %s to user %s", role.name, member.name)
-                    await member.add_roles(role)
+                    logging.info("Adding role %s to user %s", role.name, member.name)
+                    to_add.append(role)
             else:
                 if role in member.roles:
-                    logging.info("Removed role %s from user %s", role.name, member.name)
-                    await member.remove_roles(role)
+                    logging.info(
+                        "Removing role %s from user %s", role.name, member.name
+                    )
+                    to_remove.append(role)
+
+            for game, game_role in game_roles.items():
+                if mr_record:
+                    game_value = mr_record.games.get(game, False)
+                    if game_value and game_role not in member.roles:
+                        logging.info(
+                            "Adding role %s to user %s", game_role.name, member.name
+                        )
+                        to_add.append(game_role)
+                    elif not game_value and game_role in member.roles:
+                        logging.info(
+                            "Removing role %s from user %s", game_role.name, member.name
+                        )
+                        to_remove.append(game_role)
+                else:
+                    if game_role in member.roles:
+                        logging.info(
+                            "Removing role %s from user %s", game_role.name, member.name
+                        )
+                        to_remove.append(game_role)
+
+            if to_add:
+                await member.add_roles(*to_add)
+
+            if to_remove:
+                await member.remove_roles(*to_remove)
 
     @syncer.before_loop
     async def before_syncer(self):
@@ -189,6 +286,7 @@ def main():
                 bot=ghist,
                 guild_id=config["mr-sync"]["guild-id"],
                 role_id=config["mr-sync"]["role-id"],
+                game_role_ids=config["mr-sync"].get("games", {}),
             )
         )
 
